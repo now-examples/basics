cee5b528024982b5134a1b8b4da9035c1662fc03
Pranay Prakash <pranay.gp@gmail.com>
2018-08-09 01:01:29 +0000

    wait for micro to listen before starting browser

--- misc-screenshot/src/index.js
+++ misc-screenshot/src/index.js
@@ -1,40 +1,7 @@
-const { parse } = require('url');
+const micro = require('micro');
 
-const { send } = require('micro');
-const UrlPattern = require('url-pattern');
-
+const handler = require('./main');
 const { getBrowser } = require('./setup.js');
 
-const pattern = new UrlPattern('/:url', {
-  segmentValueCharset: 'a-zA-Z0-9-_./&?=%',
-});
-
-async function setup(handler) {
-  let browser = null;
-  // eslint-disable-next-line no-return-assign
-  getBrowser().then(b => browser = b);
-  return (req, res) => handler(req, res, browser);
-}
-
-module.exports = setup(async (req, res, browser) => {
-  if (req.method !== 'GET') {
-    return send(res, 501, 'Use a GET request');
-  }
-  const { pathname } = parse(req.url);
-  const params = pattern.match(pathname);
-  if (!params || !params.url) {
-    return send(res, 400, 'A url needs to be passed in like /:url');
-  }
-  if (!browser) {
-    return send(res, 503, 'Not Ready Yet. Booting up Chrome. Try again soon.');
-  }
-  if (params.url === 'favicon.ico') {
-    return send(res, 404, 'No favicon set');
-  }
-
-  console.log('Taking a screenshot of', params.url);
-  res.setHeader('Content-type', 'image/png');
-  const page = await browser.newPage();
-  await page.goto(`http://${params.url}`, { waitUntil: ['domcontentloaded', 'networkidle0'] });
-  return page.screenshot();
-});
+// pre-emptively launch chrome once the http server is up
+micro(handler).listen(3000, getBrowser);
--- misc-screenshot/src/setup.js
+++ misc-screenshot/src/setup.js
@@ -1,8 +1,4 @@
-// const fs = require('fs');
-// const tar = require('tar');
-// const zstd = require('node-zstd').decompressStream;
 const puppeteer = require('puppeteer');
-// const config = require('./config');
 
 const isBrowserAvailable = async (browser) => {
   try {
@@ -14,38 +10,25 @@ const isBrowserAvailable = async (browser) => {
   return true;
 };
 
-// const setupLocalChrome = () => new Promise((resolve, reject) => {
-// console.log(config.chromeLibSrc);
-// fs.createReadStream(config.chromeLibSrc)
-// .on('error', reject)
-// .pipe(zstd())
-// .on('error', reject)
-// .pipe(tar.x({
-// C: config.chromeLibDst,
-// }))
-// .on('error', reject)
-// .on('end', resolve);
-// });
-
-// const setupChrome = async () => {
-// console.log('setup local chrome');
-// await setupLocalChrome();
-// console.log('setup done');
-// };
-
 exports.getBrowser = (() => {
-  let browser;
+  let browser; let
+    launching;
   return async () => {
-    if (typeof browser === 'undefined' || !await isBrowserAvailable(browser)) {
-      // await setupChrome();
+    let unlock;
+    // eslint-disable-next-line no-unused-expressions
+    (launching && launching.then && await launching);
+    // eslint-disable-next-line no-return-assign
+    launching = new Promise(r => unlock = r);
+    if (!browser || !await isBrowserAvailable(browser)) {
       browser = await puppeteer.launch({
         executablePath: '/usr/bin/chromium-browser',
-        args: ['--no-sandbox', '--headless', '--disable-gpu', '-—disable-dev-tools', '--single-process'],
+        args: ['--no-sandbox', '--headless', '--disable-gpu', '-—disable-dev-tools'],
         dumpio: true,
         devtools: false,
       });
       console.log(`launch done: ${await browser.version()}`);
     }
+    unlock();
     return browser;
   };
 })();
