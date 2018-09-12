e16e507f8e8b2d60084f6c5911deae7c2a946365
Joe Cohen <joecohenr@gmail.com>
2018-09-12 01:46:00 -0500

    add rust-iron example

---
+++ rust-iron/src/main.rs
@@ -0,0 +1,14 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello from Iron")))
+    }
+
+    let _server = Iron::new(hello_world).http("0.0.0.0:3000").unwrap();
+    println!("On 3000");
+}
+
