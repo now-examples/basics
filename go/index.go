99136ff258dcc2da2f8b2549784f7097c877dc1b
Bluebeel <saikouah@gmail.com>
2018-11-08 22:59:53 +0100

    Add middleware example

--- go/index.go
+++ go/index.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+func WithLogging(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Logged connection from %s", r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	}
+}
+
+func WithTracing(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Tracing request for %s", r.RequestURI)
+		next.ServeHTTP(w, r)
+	}
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from Go on Now Lambda!")
