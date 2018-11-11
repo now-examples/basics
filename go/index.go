6355e7277fc25a1bd07cec9edb4bbeec4633c5b8
Bluebeel <saikouah@gmail.com>
2018-11-11 21:17:06 +0100

    Remove name restriction

--- go/index.go
+++ go/index.go
@@ -6,20 +6,20 @@ import (
 	"net/http"
 )
 
-func WithLogging(next http.HandlerFunc) http.HandlerFunc {
+func Logging(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Logged connection from %s", r.RemoteAddr)
 		next.ServeHTTP(w, r)
 	}
 }
 
-func WithTracing(next http.HandlerFunc) http.HandlerFunc {
+func Tracing(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Tracing request for %s", r.RequestURI)
 		next.ServeHTTP(w, r)
 	}
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from Go on Now Lambda!")
 }
