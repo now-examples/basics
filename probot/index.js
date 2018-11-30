af31f8b9c2fddb7d128f6f757e8d006aea2b9746
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-30 08:10:22 +0200

    chore: update
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- probot/index.js
+++ probot/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+if (process.env.PRIVATE_KEY) {
+  process.env.PRIVATE_KEY = Buffer.from(process.env.PRIVATE_KEY, 'base64').toString('utf8')
+}
+
+// console.log(process.env.PRIVATE_KEY)
+
 module.exports = (app) => {
   app.log('foo bar');
 };
