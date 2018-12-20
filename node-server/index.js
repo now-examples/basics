6313c89e9921797a4cc16af041101c5f82073d57
Eric Clemmons <eric@smarterspam.com>
2018-12-19 18:48:05 -0800

    Add "Listening on..." callback to node-server to help debugging

--- node-server/index.js
+++ node-server/index.js
@@ -4,4 +4,6 @@ const server = http.createServer((req, res) => {
   res.end('This is a Node.js server running on Now 2.0');
 });
 
-server.listen();
+server.listen(() => {
+  console.log(`Listening on http://localhost:${server.address().port}/`)
+});
