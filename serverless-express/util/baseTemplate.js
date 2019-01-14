b21f97f7d99c9518a0402597a508f8ac39de19d6
Matheus Fernandes <github@matheus.top>
2019-01-14 06:10:34 +0000

    Add Twitter metadata

--- serverless-express/util/baseTemplate.js
+++ serverless-express/util/baseTemplate.js
@@ -1,3 +1,3 @@
-module.exports = content => `<html lang="en">
-<head><title>Serverless Express.js with Now v2</title><meta name="viewport" content="initial-scale=1,width=device-width,user-scalable=0"/><link rel="stylesheet" href="/style.css"/></head><body>
-${content}</body></html>`;
+module.exports = (bodyContent = '' , headContent = '') => `<html lang="en">
+<head><title>Serverless Express.js with Now v2</title><meta name="viewport" content="initial-scale=1,width=device-width,user-scalable=0"/><link rel="stylesheet" href="/style.css"/>${headContent}</head><body>
+${bodyContent}</body></html>`;
