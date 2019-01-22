3a8a3cd3bd7935d44315d8709343d38dc9814d65
Allen Hai <allenhai03@gmail.com>
2019-01-21 19:04:49 -0600

    add sasazushi to availableTypesOfSushi

--- typescript/src/backend/all/index.ts
+++ typescript/src/backend/all/index.ts
@@ -1,14 +1,20 @@
 import { IncomingMessage, ServerResponse } from "http";
 
 // This could be a DB query.
-export const availableTypesOfSushi = ["maki", "temaki", "uramaki", "nigiri", "sashimi"];
+export const availableTypesOfSushi = [
+  "maki",
+  "temaki",
+  "uramaki",
+  "nigiri",
+  "sashimi",
+  "sasazushi"
+];
 
 export default (_: IncomingMessage, res: ServerResponse) => {
-
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(
     JSON.stringify({
-      data: availableTypesOfSushi,
-    }),
+      data: availableTypesOfSushi
+    })
   );
 };
