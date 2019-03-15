b1e4b73369973af498377d76a767837a784405bd
lregnier <leonardoregnier@gmail.com>
2019-03-15 18:26:45 -0300

    faunadb: updating to node serverless builder

--- faunadb/index.js
+++ faunadb/index.js
@@ -1,21 +1,15 @@
-const http = require('http');
 const faunadb = require('faunadb');
-
 const q = faunadb.query;
-const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET
-});
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
 
-const server = http.createServer((req, res) => {
+module.exports = (req, res) => {
   res.writeHeader(200, {"Content-Type": "text/html"});
-  client.query({salutation : q.Concat(["Hello ", "world."])})
-    .then((data) => {
-      res.end('This is a Node.js server running on Now 2.0. This is a FaunaDB query result: <hr> '+JSON.stringify(data));
-  }).catch((err)=>{
+  client
+      .query({salutation : q.Concat(["Hello ", "world."])})
+      .then((data) => {
+        res.end('This is a Node.js server running on Now 2.0. This is a FaunaDB query result: <hr> ' + JSON.stringify(data));
+      }).catch((err) => {
     console.error(err);
-    res.end("Error: "+JSON.stringify(err));
+    res.end("Error: " + JSON.stringify(err));
   });
-});
-
-server.listen();
-console.log('Listening on port '+server.address().port);
+}
