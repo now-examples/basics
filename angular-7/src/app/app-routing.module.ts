bb4ebc6e0e7a43836b828aa6510040bc4af620f7
Selvaganesh <selvaganeshrajam@gmail.com>
2019-03-22 19:21:54 +0530

    angular 7 example routing added

--- angular-7/src/app/app-routing.module.ts
+++ angular-7/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { HnStoryDetailedComponent } from './components/hn-story-detailed/hn-story-detailed.component';
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'item/:itemId', component: HnStoryDetailedComponent }
 ];
 
 @NgModule({
--- angular-7/src/app/app.module.ts
+++ angular-7/src/app/app.module.ts
@@ -1,23 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import {HackernewsApiService} from './services/hackernews-api.service';
+import { HnStoryItemComponent } from './components/hn-story-item/hn-story-item.component';
+import { HnStoryDetailedComponent } from './components/hn-story-detailed/hn-story-detailed.component';
+import { StoryCommentsComponent } from './components/story-comments/story-comments.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    HnStoryItemComponent,
+    HnStoryDetailedComponent,
+    StoryCommentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HackernewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
