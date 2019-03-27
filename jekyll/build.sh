d738dda148f1cccef80a8c9b81e6be71ca1c008e
timothyis <hello@timothy.is>
2019-03-27 18:35:11 +0000

    Add Jekyll example

--- jekyll/build.sh
+++ jekyll/build.sh
@@ -1,19 +1,20 @@
-yum -y install zlib zlib-devel patch readline readline-devel libyaml-devel libffi-devel openssl-devel automake libtool bison sqlite-devel curl
+yum install -y readline-devel openssl-devel zlib-devel
 
-cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
+git clone git://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
 
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-source ~/.bash_profile
+git clone git://github.com/rbenv/ruby-build.git /tmp/ruby-build
+pushd /tmp/ruby-build
+./install.sh
+popd
 
-rbenv install 2.5.5
-rbenv global 2.5.5
+rbenv install 2.5.5 && rbenv global 2.5.5
 
 gem install jekyll bundler
+
+bundle install
 
 jekyll build
 
