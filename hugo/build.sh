69ea4e73b2057f2fd9cd68511b77e926523ccd73
msweeneydev <mail@msweeneydev.com>
2019-04-04 16:29:44 +0100

    change build script

--- hugo/build.sh
+++ hugo/build.sh
@@ -3,4 +3,4 @@ yum install -y wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.31.1/hugo_0.31.1_Linux-64bit.tar.gz
 tar -xzf hugo_0.31.1_Linux-64bit.tar.gz
 
-./hugo
+./hugo --theme=ananke
