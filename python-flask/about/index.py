a759d298eb1e8909d345ab94f44dcb25af888b2a
Steven <steven@ceriously.com>
2019-04-09 12:38:31 -0400

    Add link to source code

--- python-flask/about/index.py
+++ python-flask/about/index.py
@@ -1,9 +1,11 @@
 from flask import Flask, Response, __version__
 app = Flask(__name__)
+source = 'https://github.com/zeit/now-examples/tree/master/python-flask'
+css = '<link rel="stylesheet" href="/css/style.css" />'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     name = path.upper()
     v = __version__
-    return Response("<link rel='stylesheet' href='/css/style.css' /><h1>%s</h1><ul><li>WSGI Enabled</li><li>Flask version <em>%s</em></li><ul><p>Visit the <a href='./'>home</a> page</p> or <a href='https://github.com/zeit/now-examples/tree/master/python-flask'>see the source code</a>." % (name, v), mimetype='text/html')
+    return Response("%s<h1>%s</h1><ul><li>WSGI Enabled</li><li>Flask version <em>%s</em></li></ul><p>Visit the <a href='./'>home</a> page or view the <a href='%s'>source code</a>.</p>" % (css, name, v, source), mimetype='text/html')
