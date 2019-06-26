c1f23aaf19d4cbc46aadcbd6e1cede6d52e31ab1
Adrian Carolli <adrian.carolli1@gmail.com>
2019-06-26 10:26:03 -0400

    Add os.tmpdir instead of /tmp

--- json-server/index.js
+++ json-server/index.js
@@ -1,11 +1,13 @@
 var jsonServer = require("json-server");
 var db = require("./db.js");
 var fs = require("fs");
+var os = require("os");
 
-fs.writeFileSync("/tmp/db.json", JSON.stringify(db()));
+var filePath = os.tmpdir() + "/db.json";
+fs.writeFileSync(filePath, JSON.stringify(db()));
 
 var server = jsonServer.create();
-var router = jsonServer.router("/tmp/db.json");
+var router = jsonServer.router(filePath);
 var middlewares = jsonServer.defaults();
 var port = process.env.PORT || 5000;
 
