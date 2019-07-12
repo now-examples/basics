a87b146db77a1baeb5bb2ab86bacdc829222b4f3
Adrian Carolli <adrian.carolli1@gmail.com>
2019-07-12 16:43:24 -0400

    Namespace the API for fake JSON-server

--- vanilla-json-api/api/index.js
+++ vanilla-json-api/api/index.js
@@ -1,14 +1,17 @@
-const API = require('json-server');
-const db = require('../db.js');
-const fs = require('fs');
-const os = require('os');
+const API = require("json-server");
+const db = require("./db.js");
+const routes = require("./routes.json");
+const fs = require("fs");
+const os = require("os");
 
-const filePath = os.tmpdir() + '/db.json';
+const filePath = os.tmpdir() + "/db.json";
 fs.writeFileSync(filePath, JSON.stringify(db()));
 
 const jsonAPI = API.create();
 const router = API.router(filePath);
 
+jsonAPI.use(API.rewriter(routes));
+jsonAPI.use(API.bodyParser);
 jsonAPI.use(router);
 
 module.exports = jsonAPI;
